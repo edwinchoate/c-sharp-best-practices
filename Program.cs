f1d44f3ffc0af875e9c153799f96e2c3acd817b2
Edwin Choate <edwin.choate@pm.me>
2024-12-11 13:30:10 -0500

    creates console app

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace c_sharp_best_practices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
