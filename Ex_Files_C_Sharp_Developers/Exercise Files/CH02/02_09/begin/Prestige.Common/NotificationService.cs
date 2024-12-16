f898865926982cc9bb62f930278cf61d10c38fad
Edwin Choate <edwin.choate@pm.me>
2024-12-16 10:49:10 -0500

    2.9 Static classes

---
+++ Ex_Files_C_Sharp_Developers/Exercise Files/CH02/02_09/begin/Prestige.Common/NotificationService.cs
@@ -0,0 +1,17 @@
+﻿namespace Prestige.Common
+{
+    public static class NotificationService
+    {
+
+        /// <summary>
+        /// Notifies Talent
+        /// </summary>
+        public static string NotifyTalent (string talentName)
+        {
+            string message = "talent: " + talentName;
+            Console.WriteLine(message);
+            return message;
+        }
+
+    }
+}
